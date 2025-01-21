dbc0f7a55724354964ac3023167e8cce5cf357af
juniorespriella99 <juniorespriella99@gmail.com>
2025-01-21 16:52:15 -0500

    fix: implementacion de header y configuracion de enrutamiendo a la pages home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home-pokedex',
+        component: HomeComponent,
+    },
+    {
+        path:'**',
+        redirectTo: '/home-pokedex',
+        pathMatch: 'full'
+    }
+];
+@NgModule(
+    {
+        imports: [RouterModule.forRoot(routes)],
+        exports: [RouterModule],
+    }
+) export class AppRoutingModule {}
